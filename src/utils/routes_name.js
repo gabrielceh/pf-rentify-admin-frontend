60e584921f1f82edf5e04172becc84dcd1428137
Gabriel Cervantes <76570271+gabrielceh@users.noreply.github.com>
2023-07-05 18:33:27 -0500

    routes_name products

--- src/utils/routes_name.js
+++ src/utils/routes_name.js
@@ -7,7 +7,7 @@ export const routesName = {
 		reports: '/admin/reports',
 		users: '/admin/users',
 		usersOrders: '/admin/users/order-detail',
-		products: '/admin/producs',
+		products: '/admin/products',
 		team: '/admin/team',
 		changePassword: '/admin/change-password',
 	},
